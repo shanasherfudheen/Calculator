1a5673d79cdaddfeba434db70adb6adc41a96035
shanasherfudheen <76563587+shanasherfudheen@users.noreply.github.com>
2023-10-18 21:29:57 +0530

    calculator_project

---
+++ Calculator/script.js
@@ -0,0 +1,17 @@
+function clearResult() {
+    document.getElementById("result").value = '';
+}
+
+function appendToResult(value) {
+    document.getElementById("result").value += value;
+}
+
+function calculateResult() {
+    try{
+        document.getElementById("result").value = eval(document.getElementById("result").value);
+    }
+    catch(error) {
+        document.getElementById("result").value = 'Error';
+    }
+    
+}
